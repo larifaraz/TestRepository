35e09afd6654a095e20298668ab1c3535d5f374e
Mohammad Faraz Lari <faraziraqi@Mohammads-MacBook-Air.local>
2021-10-10 12:20:22 +0530

    Python file to get metadata

--- ec2-metadata.py
+++ ec2-metadata.py
@@ -1,12 +1,22 @@
 import boto3
 
-ec2_re = boto3.resource('ec2')
+#Creating client session for EC2 service
 ec2_cl = boto3.client('ec2')
 
-for each_in in ec2_re.instances.filter(Filters=[
-    {
-        'Name': 'availability-zone',
-        'Values': ['ap-south-1c']
-    }
-]):
-    print('Instance id is {} and Instance Type is {}'.format(each_in.instance_id,each_in.instance_type))
+#Using Describe instance method to get metadata
+ec2 = ec2_cl.describe_instances()
+
+#Creating file to upload output
+f = open('ec2_metadata.json', 'w') 
+
+#Running loop to get metadata of all EC2's in account
+for each_ec2 in ec2['Reservations']:
+    # print(each_ec2)
+    # metadata = each_ec2
+    f.write(str(each_ec2))
+    f.close()
+
+
+
+
+
